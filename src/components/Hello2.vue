902bb18e32c5f51ee894532086b64186460b651d
unknown <huangyexin@BX-HUANGYEXIN.bxjr.com>
2016-08-03 17:50:50 +0800

    create

---
+++ src/components/Hello2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1 v-on:click="letgo">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!2222'
+    }
+  },
+  methods:{
+    letgo: function () {
+      this.$route.router.go({ path: '/hello' })
+    }
+  },
+  created:function(){
+   console.log("created hello2")
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
